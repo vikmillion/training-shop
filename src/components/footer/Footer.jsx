9b0e527c91f06b710a9bc4db238f2b82f6320f81
vikmillion <vikmillion@gmail.com>
2022-02-24 16:29:14 +0300

    test2

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -90,43 +90,26 @@ function Footer(props) {
     <>
       <div className={classes.footer} data-test-id="footer">
         {/* <div className={classes.containerBox}> */}
-          <div className={classes.blockBlack}>
-            {/* <div className={classes.div1}> */}
-            <div className="beInTouch"> BE IN TOUCH WITH US:</div> 
-              <input type="text" placeholder="Enter you email" />
-              <div>
-                <button className={classes.btn}> JOIN US</button>
-              </div>{" "}
-              <div className={classes.blockImg}>
-                <img src={facebook} alt="" />
-                <img src={twitter} alt="" />
-                <img src={instargam} alt="" />
-                <img src={pinterest} alt="" />
-              {/* </div> */}
-            </div>
-          </div>
-          <div className={classes.linkGroup}>
-            <div className={classes.div3}>
-              <div className={classes.upper}>CATEGORIES</div>
-              <div>
-                {linkes1.map((i) => (
-                  <>
-                    <Link
-                      to={i.path}
-                      data-test-id={`footer-nav-link-${i.path}`}
-                      className={classes.linkUnder}
-                    >
-                      {i.title}
-                    </Link>{" "}
-                    <br></br>
-                  </>
-                ))}
-              </div>
-            </div>
 
-            <div className={classes.div3}>
-              <div className={classes.upper}>Information</div>
-              {linkes2.map((i) => (
+        <div className={classes.blockBlack}>
+          <div className="beInTouch"> BE IN TOUCH WITH US:</div>
+          <input type="text" placeholder="Enter you email" />
+          <div>
+            <button className={classes.btn}> JOIN US</button>
+          </div>{" "}
+          <div className={classes.blockImg}>
+            <img src={facebook} alt="" />
+            <img src={twitter} alt="" />
+            <img src={instargam} alt="" />
+            <img src={pinterest} alt="" />
+          </div>
+        </div>
+
+        <div className={classes.linkGroup}>
+          <div className={classes.div3}>
+            <div className={classes.upper}>CATEGORIES</div>
+            <div>
+              {linkes1.map((i) => (
                 <>
                   <Link
                     to={i.path}
@@ -135,49 +118,67 @@ function Footer(props) {
                   >
                     {i.title}
                   </Link>{" "}
-                  <br></br>
+                
                 </>
               ))}
             </div>
-            <div className={classes.div3}>
-              <div className={classes.upper}>USEFUL LINK </div>
-              {linkes3.map((i) => (
-                <>
-                  <Link
-                    to={i.path}
-                    data-test-id={`footer-nav-link-${i.path}`}
-                    className={classes.linkUnder}
-                  >
-                    {i.title}
-                  </Link>{" "}
-                  <br></br>
-                </>
-              ))}
-            </div>
-            <div className={classes.div3}>
-              <div className={classes.upper}>CONTACT US</div>
-              <div className={classes.linkUnder}>
-                <img src={place} alt="" /> Belarus, Gomel, Lange 17 <br />
-                <img src={call} alt="" /> +375 29 100 20 30 <br />
-                <img src={clock} alt="" /> All week 24/7 <br />
-                <img src={letter} alt="" />{" "}
-                <span className={classes.underline}> info@clevertec.ru</span>
-              </div>
+          </div>
+
+          <div className={classes.div3}>
+            <div className={classes.upper}>Information</div>
+            {linkes2.map((i) => (
+              <>
+                <Link
+                  to={i.path}
+                  data-test-id={`footer-nav-link-${i.path}`}
+                  className={classes.linkUnder}
+                >
+                  {i.title}
+                </Link>{" "}
+               
+              </>
+            ))}
+          </div>
+          <div className={classes.div3}>
+            <div className={classes.upper}>USEFUL LINK </div>
+            {linkes3.map((i) => (
+              <>
+                <Link
+                  to={i.path}
+                  data-test-id={`footer-nav-link-${i.path}`}
+                  className={classes.linkUnder}
+                >
+                  {i.title}
+                </Link>{" "}
+            
+              </>
+            ))}
+          </div>
+          <div className={classes.div3}>
+            <div className={classes.upper}>CONTACT US</div>
+            <div className={classes.linkUnder_4}>
+              <div><img src={place} alt="" /> Belarus, Gomel, Lange 17 <br /></div>
+              <div><img src={call} alt="" /> +375 29 100 20 30 <br /></div>
+              <div><img src={clock} alt="" /> All week 24/7 <br /></div>
+            <div>  <img src={letter} alt="" />{" "}
+              <span className={classes.underline}> info@clevertec.ru</span></div> 
             </div>
           </div>
-          <div className={classes.blockBottom}>
-            <div>Copyright © 2032 all rights reserved</div>
-            <div className={classes.blockImg2}>
-              <img src={stripe} alt="" />
-              <img src={aes} alt="" />
-              <img src={paypal} alt="" />
-              <img src={visa} alt="" />
-              <img src={mastercard} alt="" />
-              <img src={discover} alt="" />
-              <img src={american} alt="" />
-            </div>
-            <div className={classes.underline}>Clevertec.ru/training</div>
+        </div>
+
+        <div className={classes.blockBottom}>
+          <div>Copyright © 2032 all rights reserved</div>
+          <div className={classes.blockImg2}>
+            <img src={stripe} alt="" />
+            <img src={aes} alt="" />
+            <img src={paypal} alt="" />
+            <img src={visa} alt="" />
+            <img src={mastercard} alt="" />
+            <img src={discover} alt="" />
+            <img src={american} alt="" />
           </div>
+          <div className={classes.underline}>Clevertec.ru/training</div>
+        </div>
         {/* </div> */}
       </div>
     </>
